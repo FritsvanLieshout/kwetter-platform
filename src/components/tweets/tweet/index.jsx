5ba05fa9615747312e6be20c104702b27bcae3c0
Frits van Lieshout <fritske05@gmail.com>
2021-03-24 18:19:19 +0100

    Removed console log

--- src/components/tweets/tweet/index.jsx
+++ src/components/tweets/tweet/index.jsx
@@ -1,54 +1,11 @@
 import React, { Component } from "react";
 import "./index.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import moment from "moment";
 
 class KwetterComponentTweet extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      //difference: 0,
-    };
   }
-
-  // getDatePosted(date) {
-  //   var dateNow = new Date();
-  //   var datePosted = new Date(date);
-  //   console.log(dateNow, datePosted);
-  //   let difference;
-
-  //   var delta = Math.abs(dateNow - datePosted) / 1000;
-
-  //   var days = Math.floor(delta / 86400);
-  //   delta -= days * 86400;
-
-  //   var hours = Math.floor(delta / 3600) % 24;
-  //   delta -= hours * 3600;
-
-  //   var minutes = Math.floor(delta / 60) % 60;
-  //   delta -= minutes * 60;
-
-  //   var seconds = delta % 60;
-
-  //   if (days > 0) {
-  //     difference = days + " d";
-  //   }
-
-  //   if (!difference && hours > 0) {
-  //     difference = hours + " u";
-  //   }
-
-  //   if (!difference && minutes > 0) {
-  //     difference = minutes + " m";
-  //   }
-
-  //   if (!difference) {
-  //     difference = seconds.toFixed(0) + " s";
-  //   }
-
-  //   return difference;
-  //   //return difference;
-  // }
 
   getDatePosted(posted) {
     let difference = 0;
@@ -127,14 +84,8 @@ class KwetterComponentTweet extends Component {
     return diffInMs / 1000;
   }
 
-  componentDidMount() {
-    //let diff = this.getDatePosted(this.props.tweet.posted);
-    this.setState({ dateNow: new Date() });
-  }
-
   render() {
     let { tweet } = this.props;
-    let { dateNow } = this.state;
 
     return (
       <div>
